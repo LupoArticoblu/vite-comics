d0a070bd35234b640afd73f061b74333f4e18791
LupoArticoblu <wolf94-@libero.it>
2022-11-16 17:41:25 +0100

    Rivisitazione progetto props step1

--- vite-dc-comics/src/App.vue
+++ vite-dc-comics/src/App.vue
@@ -1,12 +1,34 @@
+<script>
+  import Villain from "./components/Villain.vue";
+  import Heroes from "./components/Heroes.vue"
+
+  export default {
+   // name: App è una convenzione 
+   name: "App",
+   //registrazione dei componenti
+   components:{
+    supereroi,
+    supercattivi
+   },
+   data(){
+    return {
+      Eroe1: "Superman",
+      Eroe2: "Batman",
+      Eroe3: "Wonder Woman",
+
+      Cattivo1: "Doomsday",
+      Cattivo2: "Joker",
+      Cattivo3: "Morbius"
+
+    }
+   }
+  }
+</script>
+
 <template>
   
 </template>
 
-<script>
-export default {
-
-}
-</script>
 
 <style>
 
